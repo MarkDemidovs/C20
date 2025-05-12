ef8e5ecd0dc0f2f82233fb0bcdbc41c3765a70c2
Mark <markdemidovs@proton.me>
2025-05-12 22:38:22 +0300

    day 1 uploads

---
+++ genesis1.c
@@ -0,0 +1,7 @@
+#include <stdio.h> 
+int main () {
+	printf("1 In the beginning God created the heavens and the earth. \n 2 Now the earth was formless and empty, darkness was over the surface of the deep, and the Spirit of God was hovering over the waters. \n");
+	printf("3 And God said, “Let there be light,” and there was light. \n4 God saw that the light was good, and he separated the light from the darkness. \n5 God called the light “day,” and the darkness he called “night.” And there was evening, and there was morning—the first day.");
+
+	return 0;
+}
